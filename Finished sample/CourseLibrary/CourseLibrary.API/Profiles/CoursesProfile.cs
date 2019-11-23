75c92f914ff5d3d3bddeba2931e237b95dae349f
glad4enkonm <glad4enkonm@gmail.com>
2019-11-23 17:23:23 +0100

    reverse map added for courses profile

--- Finished sample/CourseLibrary/CourseLibrary.API/Profiles/CoursesProfile.cs
+++ Finished sample/CourseLibrary/CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -12,8 +12,7 @@ namespace CourseLibrary.API.Profiles
         {
             CreateMap<Entities.Course, Models.CourseDto>();
             CreateMap<Models.CourseForCreationDto, Entities.Course>();
-            CreateMap<Models.CourseForUpdateDto, Entities.Course>();
-            CreateMap<Entities.Course, Models.CourseForUpdateDto>();
+            CreateMap<Models.CourseForUpdateDto, Entities.Course>().ReverseMap();
         }
     }
 }
